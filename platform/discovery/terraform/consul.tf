b032efeb8ccc646c55b63e4c831e7b08dee1cd06
louis gueye <louis.gueye@gmail.com>
2019-06-09 13:57:14 +0200

    Fixes #1

--- platform/discovery/terraform/consul.tf
+++ platform/discovery/terraform/consul.tf
@@ -1,3 +1,9 @@
+# apply usage
+# terraform apply -var "do_token=${DO_API_TOKEN}" -var "pub_key=${HOME}/.ssh/id_rsa.pub" -var "pvt_key=${HOME}/.ssh/id_rsa" -var "ssh_fingerprint=`ssh-keygen -lf ~/.ssh/id_rsa.pub -E md5  | awk '{ print $2 }' | cut -c 5-`" -var "target_env=staging"
+
+# destroy usage
+# terraform destroy -var "do_token=${DO_API_TOKEN}" -var "pub_key=${HOME}/.ssh/id_rsa.pub" -var "pvt_key=${HOME}/.ssh/id_rsa" -var "ssh_fingerprint=`ssh-keygen -lf ~/.ssh/id_rsa.pub -E md5  | awk '{ print $2 }' | cut -c 5-`" -var "target_env=staging"
+
 # consul cluster vars
 variable "consul_client_role" {
   default = "consul-client"
