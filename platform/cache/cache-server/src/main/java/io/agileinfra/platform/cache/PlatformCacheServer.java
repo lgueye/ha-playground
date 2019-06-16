6a6502637a6bcf49abe9390e312ef99bf1f69fd5
louis gueye <louis.gueye@gmail.com>
2019-06-16 18:21:57 +0200

    fixes #5

---
+++ platform/cache/cache-server/src/main/java/io/agileinfra/platform/cache/PlatformCacheServer.java
@@ -0,0 +1,14 @@
+package io.agileinfra.platform.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author louis.gueye@gmail.com
+ */
+@SpringBootApplication
+public class PlatformCacheServer {
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformCacheServer.class, args);
+	}
+}
