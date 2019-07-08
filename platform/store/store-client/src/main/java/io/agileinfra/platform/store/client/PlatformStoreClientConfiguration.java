8d6153b4ab2764249b07fa62144d49a80128461a
louis gueye <louis.gueye@gmail.com>
2019-07-08 21:07:54 +0200

    create store-client

---
+++ platform/store/store-client/src/main/java/io/agileinfra/platform/store/client/PlatformStoreClientConfiguration.java
@@ -0,0 +1,10 @@
+package io.agileinfra.platform.store.client;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author louis.gueye@gmail.com
+ */
+@Configuration
+public class PlatformStoreClientConfiguration {
+}
