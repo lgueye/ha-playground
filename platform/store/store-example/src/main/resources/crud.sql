f7b3974b5e9cbb297c8502ac53725d39585238fb
louis.gueye <louis.gueye@domo-safety.com>
2019-07-09 10:30:23 +0200

    fix crud.sql

--- platform/store/store-example/src/main/resources/crud.sql
+++ platform/store/store-example/src/main/resources/crud.sql
@@ -3,7 +3,7 @@ insert into example_events_timeline (event_id, state, inserted_at, timestamp, se
 insert into example_events_timeline (event_id, state, inserted_at, timestamp, sensor_bid) values ('5b68570d-7593-4310-8ad1-3fee88424ff8', 'on', 1562591905000, 1562591905000, 'sens-q7ikjxk1ftik');
 insert into example_events_timeline (event_id, state, inserted_at, timestamp, sensor_bid) values ('47abd6c3-bdff-46dc-877d-8f5284659a0a', 'off', 1562591906000, 1562591906000, 'sens-q7ikjxk1ftik');
 
-select * from example_events_timeline where sensor_bid = 'sens-q7ikjxk1ftik' state = 'off';
+select * from example_events_timeline where sensor_bid = 'sens-q7ikjxk1ftik' and state = 'off';
 
 update example_events_timeline set state = 'on' where state = 'off';
 
