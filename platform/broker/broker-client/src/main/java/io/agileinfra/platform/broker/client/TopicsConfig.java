de7ecd76c3f8a023665d855ef316edb01a9b6dbd
louis.gueye <louis.gueye@domo-safety.com>
2019-07-11 09:12:06 +0200

    fix lombok annotations usage

--- platform/broker/broker-client/src/main/java/io/agileinfra/platform/broker/client/TopicsConfig.java
+++ platform/broker/broker-client/src/main/java/io/agileinfra/platform/broker/client/TopicsConfig.java
@@ -1,10 +1,7 @@
 package io.agileinfra.platform.broker.client;
 
 import com.google.common.collect.Sets;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 import java.util.Set;
 
@@ -12,9 +9,6 @@ import java.util.Set;
  * Created by <a href="mailto:louis.gueye@domo-safety.com">Louis Gueye</a>.
  */
 @Data
-@RequiredArgsConstructor
-@AllArgsConstructor
-@NoArgsConstructor
 public class TopicsConfig {
 	private Set<ExchangeDto> exchanges = Sets.newHashSet();
 }
