d17867517ff3812f772f992a21609849f0b31822
louis gueye <louis.gueye@gmail.com>
2019-07-11 23:24:13 +0200

    declare job :(

--- platform/broker/broker-example-producer/src/main/java/io/agileinfra/platform/broker/example/producer/PlatformBrokerExampleProducerConfiguration.java
+++ platform/broker/broker-example-producer/src/main/java/io/agileinfra/platform/broker/example/producer/PlatformBrokerExampleProducerConfiguration.java
@@ -1,6 +1,8 @@
 package io.agileinfra.platform.broker.example.producer;
 
 import io.agileinfra.platform.broker.client.PlatformBrokerClientConfiguration;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -11,4 +13,8 @@ import org.springframework.context.annotation.Import;
 @Import(PlatformBrokerClientConfiguration.class)
 public class PlatformBrokerExampleProducerConfiguration {
 
+    @Bean
+    public PlatformBrokerExampleProducerJob platformBrokerExampleProducerJob(final AmqpTemplate amqpTemplate) {
+        return new PlatformBrokerExampleProducerJob(amqpTemplate);
+    }
 }
