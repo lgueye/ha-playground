c4f06d07e6c470746bcc1b30543be0be4f0b3c13
louis.gueye <louis.gueye@domo-safety.com>
2019-07-11 17:13:12 +0200

    fix spring config

--- platform/broker/broker-client/src/main/java/io/agileinfra/platform/broker/client/PlatformBrokerClientConfiguration.java
+++ platform/broker/broker-client/src/main/java/io/agileinfra/platform/broker/client/PlatformBrokerClientConfiguration.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.google.common.collect.Lists;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Binding;
@@ -19,7 +18,6 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import java.util.List;
@@ -34,8 +32,6 @@ import java.util.List;
  */
 @Configuration
 @Slf4j
-@RequiredArgsConstructor
-@Import({TopicsConfig.class, QueuesConfig.class})
 public class PlatformBrokerClientConfiguration {
 
 	@Bean
@@ -51,6 +47,15 @@ public class PlatformBrokerClientConfiguration {
 	}
 
 	@Bean
+	public QueuesConfig queuesConfig() {
+		return new QueuesConfig();
+	}
+	@Bean
+	public TopicsConfig topicsConfig() {
+		return new TopicsConfig();
+	}
+
+	@Bean
 	public List<Declarable> directBindings(final AmqpAdmin amqpAdmin, final QueuesConfig queuesConfig) {
 		log.info("Creating Destinations...");
 		final List<Declarable> declarables = Lists.newArrayList();
@@ -61,11 +66,11 @@ public class PlatformBrokerClientConfiguration {
 			exchange.getRoutes().forEach(queue -> {
 				Queue q = QueueBuilder.durable(queue.getId()).build();
 				log.info("Successfully created queue {}.", queue.getId());
-                amqpAdmin.declareQueue(q);
+				amqpAdmin.declareQueue(q);
 				declarables.add(q);
 				Binding b = BindingBuilder.bind(q).to(ex).with(queue.getKey()).noargs();
 				declarables.add(b);
-                amqpAdmin.declareBinding(b);
+				amqpAdmin.declareBinding(b);
 				log.info("Successfully bound exchange {} to queue {} with routing key {}.", exchange.getId(), queue.getId(), queue.getKey());
 			});
 		});
@@ -79,14 +84,14 @@ public class PlatformBrokerClientConfiguration {
 		topicsConfig.getExchanges().forEach(exchange -> {
 			Exchange ex = ExchangeBuilder.fanoutExchange(exchange.getId()).durable(true).build();
 			declarables.add(ex);
-            amqpAdmin.declareExchange(ex);
+			amqpAdmin.declareExchange(ex);
 			exchange.getRoutes().forEach(queue -> {
 				Queue q = QueueBuilder.nonDurable(queue.getId()).autoDelete().exclusive().build();
 				log.info("Successfully created queue {}.", q.getName());
 				declarables.add(q);
-                amqpAdmin.declareQueue(q);
+				amqpAdmin.declareQueue(q);
 				Binding b = BindingBuilder.bind(q).to(ex).with(queue.getKey()).noargs();
-                amqpAdmin.declareBinding(b);
+				amqpAdmin.declareBinding(b);
 				declarables.add(b);
 				log.info("Successfully bound exchange {} to queue {} with routing key {}.", ex.getName(), q.getName(), b.getRoutingKey());
 			});
--- platform/broker/broker-client/src/main/java/io/agileinfra/platform/broker/client/TopicsConfig.java
+++ platform/broker/broker-client/src/main/java/io/agileinfra/platform/broker/client/TopicsConfig.java
@@ -1,18 +1,67 @@
 package io.agileinfra.platform.broker.client;
 
-import com.google.common.collect.Sets;
-import lombok.Data;
+import com.google.common.collect.Lists;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import java.util.Set;
+import java.util.List;
 
 /**
  * Created by <a href="mailto:louis.gueye@domo-safety.com">Louis Gueye</a>.
  */
 @Component
-@Data
-@ConfigurationProperties("topics")
+@ConfigurationProperties(prefix = "topics")
 public class TopicsConfig {
-	private Set<ExchangeDto> exchanges = Sets.newHashSet();
+	private List<ExchangeDto> exchanges;
+
+	public List<ExchangeDto> getExchanges() {
+		return exchanges;
+	}
+
+	public void setExchanges(List<ExchangeDto> exchanges) {
+		this.exchanges = exchanges;
+	}
+
+	public static class ExchangeDto {
+		private String id;
+		private List<RoutingDto> routes = Lists.newArrayList();
+
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public List<RoutingDto> getRoutes() {
+			return routes;
+		}
+
+		public void setRoutes(List<RoutingDto> routes) {
+			this.routes = routes;
+		}
+
+		public static class RoutingDto {
+			private String id;
+			private String key;
+
+			public String getId() {
+				return id;
+			}
+
+			public void setId(String id) {
+				this.id = id;
+			}
+
+			public String getKey() {
+				return key;
+			}
+
+			public void setKey(String key) {
+				this.key = key;
+			}
+		}
+	}
+
 }
