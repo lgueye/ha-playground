4586093b490303fab1e7f093e0f574860f54956b
louis.gueye <louis.gueye@domo-safety.com>
2019-07-11 09:23:30 +0200

    try different config for queues/topic declaration

--- platform/broker/broker-client/src/main/java/io/agileinfra/platform/broker/client/TopicsConfig.java
+++ platform/broker/broker-client/src/main/java/io/agileinfra/platform/broker/client/TopicsConfig.java
@@ -2,12 +2,18 @@ package io.agileinfra.platform.broker.client;
 
 import com.google.common.collect.Sets;
 import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import java.util.Set;
 
 /**
  * Created by <a href="mailto:louis.gueye@domo-safety.com">Louis Gueye</a>.
  */
+@Configuration
+@ConfigurationProperties("topics")
+@PropertySource("file:///var/platform/current/broker-example-consumer.yaml")
 @Data
 public class TopicsConfig {
 	private Set<ExchangeDto> exchanges = Sets.newHashSet();
