d2f381f8bb7cca5a8f90455699b68fe4f3a9e0cd
louis.gueye <louis.gueye@domo-safety.com>
2019-07-11 09:08:57 +0200

    fix configuration property usage

--- platform/broker/broker-client/src/main/java/io/agileinfra/platform/broker/client/TopicsConfig.java
+++ platform/broker/broker-client/src/main/java/io/agileinfra/platform/broker/client/TopicsConfig.java
@@ -1,18 +1,20 @@
 package io.agileinfra.platform.broker.client;
 
 import com.google.common.collect.Sets;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import java.util.Set;
 
 /**
  * Created by <a href="mailto:louis.gueye@domo-safety.com">Louis Gueye</a>.
  */
-@Component
-@ConfigurationProperties("topics")
 @Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class TopicsConfig {
 	private Set<ExchangeDto> exchanges = Sets.newHashSet();
 }
