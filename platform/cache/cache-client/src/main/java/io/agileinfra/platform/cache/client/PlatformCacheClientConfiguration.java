dba05a5b7ec3f0ac7608ec25281d0ac86c53869a
louis gueye <louis.gueye@gmail.com>
2019-07-13 17:15:50 +0200

    create hazelcast config

--- platform/cache/cache-client/src/main/java/io/agileinfra/platform/cache/client/PlatformCacheClientConfiguration.java
+++ platform/cache/cache-client/src/main/java/io/agileinfra/platform/cache/client/PlatformCacheClientConfiguration.java
@@ -1,7 +1,15 @@
 package io.agileinfra.platform.cache.client;
 
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.config.XmlClientConfigBuilder;
+import com.hazelcast.core.HazelcastInstance;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @author louis.gueye@gmail.com
@@ -9,4 +17,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @Slf4j
 public class PlatformCacheClientConfiguration {
-}
+    @Bean
+    public HazelcastInstance getHazelcastClientInstance(@Value("${spring.hazelcast.config}") final File configFile) throws IOException {
+        ClientConfig clientConfig = new XmlClientConfigBuilder(configFile).build();
+        return com.hazelcast.client.HazelcastClient.newHazelcastClient(clientConfig);
+    }}
