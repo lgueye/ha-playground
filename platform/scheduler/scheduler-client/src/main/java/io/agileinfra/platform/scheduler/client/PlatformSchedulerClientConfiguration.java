2bd2835842daed4e34cb43b54eeb7b8f34a7deb5
louis.gueye <louis.gueye@domo-safety.com>
2019-07-18 13:32:50 +0200

    backend_scheduler_client

--- platform/scheduler/scheduler-client/src/main/java/io/agileinfra/platform/scheduler/client/PlatformSchedulerClientConfiguration.java
+++ platform/scheduler/scheduler-client/src/main/java/io/agileinfra/platform/scheduler/client/PlatformSchedulerClientConfiguration.java
@@ -1,12 +1,15 @@
 package io.agileinfra.platform.scheduler.client;
 
+import io.agileinfra.platform.broker.client.PlatformBrokerClientConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author louis.gueye@gmail.com
  */
 @Configuration
+@Import(PlatformBrokerClientConfiguration.class)
 @Slf4j
 public class PlatformSchedulerClientConfiguration {
 }
