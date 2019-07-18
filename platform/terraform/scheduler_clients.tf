903f20f3cbcbd8adcc94a22a97f86eb1c696e7ac
louis.gueye <louis.gueye@domo-safety.com>
2019-07-18 13:31:52 +0200

    terraform_scheduler_servers_requirements

---
+++ platform/terraform/scheduler_clients.tf
@@ -0,0 +1,39 @@
+# scheduler clients vars
+variable "scheduler_client_role" {
+  default = "scheduler-client"
+}
+variable "scheduler_producer_role" {
+  default = "scheduler-producer"
+}
+
+resource "digitalocean_tag" "scheduler_client_role" {
+  name = "${var.scheduler_client_role}"
+}
+resource "digitalocean_tag" "scheduler_producer_role" {
+  name = "${var.scheduler_producer_role}"
+}
+
+# scheduler client droplets and ansible inventory
+
+# producers
+resource "digitalocean_droplet" "scheduler_producer_01_droplet" {
+  image = "${var.droplet_image}"
+  name = "${var.scheduler_producer_role}-01"
+  region = "${var.primary_datacenter_name}"
+  size = "${var.droplet_size}"
+  private_networking = true
+  ssh_keys = ["${var.ssh_fingerprint}"]
+  tags = ["${var.target_env}","${var.scheduler_client_role}","${var.scheduler_producer_role}","${var.discovery_client_role}"]
+}
+
+resource "ansible_host" "scheduler_producer_droplet" {
+  inventory_hostname = "${digitalocean_droplet.scheduler_producer_01_droplet.name}"
+  groups = ["${var.target_env}","${var.discovery_client_role}","${var.scheduler_client_role}","${var.scheduler_producer_role}"]
+  vars {
+    ansible_host = "${digitalocean_droplet.scheduler_producer_01_droplet.ipv4_address}"
+    ansible_python_interpreter = "${var.ansible_python_interpreter}"
+    datacenter_name = "${var.primary_datacenter_name}"
+    datacenter_role = "${var.primary_datacenter_role}"
+  }
+}
+
