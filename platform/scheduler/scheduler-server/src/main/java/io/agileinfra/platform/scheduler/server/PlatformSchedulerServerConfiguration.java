852862839e98234e85d3a837e0338a0ed7b65b91
louis gueye <louis.gueye@gmail.com>
2019-07-19 10:17:59 +0200

    fix broker configuration

--- platform/scheduler/scheduler-server/src/main/java/io/agileinfra/platform/scheduler/server/PlatformSchedulerServerConfiguration.java
+++ platform/scheduler/scheduler-server/src/main/java/io/agileinfra/platform/scheduler/server/PlatformSchedulerServerConfiguration.java
@@ -2,6 +2,7 @@ package io.agileinfra.platform.scheduler.server;
 
 import com.google.common.collect.Lists;
 import io.agileinfra.platform.broker.client.PlatformBrokerClientConfiguration;
+import io.agileinfra.platform.broker.client.QueuesConfig;
 import io.agileinfra.platform.broker.client.TopicsConfig;
 import io.agileinfra.platform.cache.client.PlatformCacheClientConfiguration;
 import lombok.extern.slf4j.Slf4j;
@@ -30,6 +31,34 @@ public class PlatformSchedulerServerConfiguration {
 		return Executors.newScheduledThreadPool(3);
 	}
 
+
+	@Bean
+	public QueuesConfig queuesConfig() {
+		return new QueuesConfig();
+	}
+
+	@Bean
+	public List<Declarable> directBindings(final AmqpAdmin amqpAdmin, final QueuesConfig queuesConfig) {
+		log.info("Creating Destinations...");
+		final List<Declarable> declarables = Lists.newArrayList();
+		queuesConfig.getExchanges().forEach(exchange -> {
+			Exchange ex = ExchangeBuilder.directExchange(exchange.getId()).durable(true).build();
+			amqpAdmin.declareExchange(ex);
+			declarables.add(ex);
+			exchange.getRoutes().forEach(queue -> {
+				Queue q = QueueBuilder.durable(queue.getId()).build();
+				log.info("Successfully created queue {}.", queue.getId());
+				amqpAdmin.declareQueue(q);
+				declarables.add(q);
+				Binding b = BindingBuilder.bind(q).to(ex).with(queue.getKey()).noargs();
+				declarables.add(b);
+				amqpAdmin.declareBinding(b);
+				log.info("Successfully bound exchange {} to queue {} with routing key {}.", exchange.getId(), queue.getId(), queue.getKey());
+			});
+		});
+		return declarables;
+	}
+
 	@Bean
 	public TopicsConfig topicsConfig() {
 		return new TopicsConfig();
@@ -44,7 +73,7 @@ public class PlatformSchedulerServerConfiguration {
 			declarables.add(ex);
 			amqpAdmin.declareExchange(ex);
 			exchange.getRoutes().forEach(queue -> {
-				Queue q = QueueBuilder.nonDurable(queue.getId()).autoDelete().build();
+				Queue q = QueueBuilder.nonDurable().autoDelete().build();
 				log.info("Successfully created queue {}.", q.getName());
 				declarables.add(q);
 				amqpAdmin.declareQueue(q);
