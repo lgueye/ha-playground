6eee7507e99d9510293a53c27c7228e0a4e615a1
louis.gueye <louis.gueye@domo-safety.com>
2019-07-31 12:34:25 +0200

    try different setup for rabbitmq cluster config

--- platform/terraform/broker_clients.tf
+++ platform/terraform/broker_clients.tf
@@ -21,29 +21,56 @@ resource "digitalocean_tag" "broker_producer_role" {
 
 # broker client droplets and ansible inventory
 
-# consumer
-resource "digitalocean_droplet" "broker_consumer_droplet" {
+# consumers
+resource "digitalocean_droplet" "broker_consumer_01_droplet" {
   image = "${var.droplet_image}"
-  name = "${var.broker_consumer_role}"
+  name = "${var.broker_consumer_role}-01"
   region = "${var.primary_datacenter_name}"
   size = "${var.droplet_size}"
   private_networking = true
   ssh_keys = ["${var.ssh_fingerprint}"]
-  tags = ["${var.target_env}","${var.broker_client_role}","${var.broker_consumer_role}","${var.discovery_client_role}"]
+  tags = ["${var.target_env}","${var.broker_client_role}","${var.broker_consumer_role}","${var.discovery_client_role}","${var.java_runtime_role}"]
 }
 
-resource "ansible_host" "broker_consumer_droplet" {
-  inventory_hostname = "${digitalocean_droplet.broker_consumer_droplet.name}"
+resource "ansible_host" "broker_consumer_01_droplet" {
+  inventory_hostname = "${digitalocean_droplet.broker_consumer_01_droplet.name}"
   groups = [
     "${var.target_env}",
     "${var.discovery_client_role}",
     "${var.broker_client_role}",
-    "${var.broker_consumer_role}"]
+    "${var.broker_consumer_role}",
+    "${var.java_runtime_role}"]
   vars {
-    ansible_host = "${digitalocean_droplet.broker_consumer_droplet.ipv4_address}"
+    ansible_host = "${digitalocean_droplet.broker_consumer_01_droplet.ipv4_address}"
     ansible_python_interpreter = "${var.ansible_python_interpreter}"
     datacenter_name = "${var.primary_datacenter_name}"
     datacenter_role = "${var.primary_datacenter_role}"
+  }
+}
+
+resource "digitalocean_droplet" "broker_consumer_02_droplet" {
+  image = "${var.droplet_image}"
+  name = "${var.broker_consumer_role}-02"
+  region = "${var.primary_datacenter_name}"
+  size = "${var.droplet_size}"
+  private_networking = true
+  ssh_keys = ["${var.ssh_fingerprint}"]
+  tags = ["${var.target_env}","${var.broker_client_role}","${var.broker_consumer_role}","${var.discovery_client_role}","${var.java_runtime_role}"]
+}
+
+resource "ansible_host" "broker_consumer_02_droplet" {
+  inventory_hostname = "${digitalocean_droplet.broker_consumer_02_droplet.name}"
+  groups = [
+    "${var.target_env}",
+    "${var.discovery_client_role}",
+    "${var.broker_client_role}",
+    "${var.broker_consumer_role}",
+    "${var.java_runtime_role}"]
+  vars {
+    ansible_host = "${digitalocean_droplet.broker_consumer_02_droplet.ipv4_address}"
+    ansible_python_interpreter = "${var.ansible_python_interpreter}"
+    datacenter_name = "${var.fallback_datacenter_name}"
+    datacenter_role = "${var.fallback_datacenter_name}"
   }
 }
 
@@ -55,12 +82,12 @@ resource "digitalocean_droplet" "broker_producer_droplet" {
   size = "${var.droplet_size}"
   private_networking = true
   ssh_keys = ["${var.ssh_fingerprint}"]
-  tags = ["${var.target_env}","${var.broker_client_role}","${var.broker_producer_role}","${var.discovery_client_role}"]
+  tags = ["${var.target_env}","${var.broker_client_role}","${var.broker_producer_role}","${var.discovery_client_role}","${var.java_runtime_role}"]
 }
 
 resource "ansible_host" "broker_producer_droplet" {
   inventory_hostname = "${digitalocean_droplet.broker_producer_droplet.name}"
-  groups = ["${var.target_env}","${var.discovery_client_role}","${var.broker_client_role}","${var.broker_producer_role}"]
+  groups = ["${var.target_env}","${var.discovery_client_role}","${var.broker_client_role}","${var.broker_producer_role}","${var.java_runtime_role}"]
   vars {
     ansible_host = "${digitalocean_droplet.broker_producer_droplet.ipv4_address}"
     ansible_python_interpreter = "${var.ansible_python_interpreter}"
